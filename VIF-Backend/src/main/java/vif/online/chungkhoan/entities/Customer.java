d4f8cb401d48c214384a71a6d509ad7906c45f5b
Huu Tuyen <huutuyen91@gmail.com>
2019-02-25 23:02:03 +0700

    add customer entity

---
+++ VIF-Backend/src/main/java/vif/online/chungkhoan/entities/Customer.java
@@ -0,0 +1,104 @@
+package vif.online.chungkhoan.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "USER_NAME", nullable = false)
+	private String username;
+
+	@Column(name = "FULL_NAME", nullable = false)
+	private String fullName;
+
+	@Column(name = "AVATAR")
+	private String avatar;
+
+	@Column(name = "EMAIL")
+	private String email;
+
+	@Column(name = "TOTAL_CCQ")
+	private BigDecimal totalCcq;
+
+	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<User> users = new ArrayList<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public BigDecimal getTotalCcq() {
+		return totalCcq;
+	}
+
+	public void setTotalCcq(BigDecimal totalCcq) {
+		this.totalCcq = totalCcq;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+}
--- VIF-Backend/src/main/java/vif/online/chungkhoan/entities/User.java
+++ VIF-Backend/src/main/java/vif/online/chungkhoan/entities/User.java
@@ -4,9 +4,12 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -44,6 +47,10 @@ public class User implements Serializable{
     
     @Column(name = "IS_DELETED")
     private Integer isDeleted;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
 
 	public Long getId() {
 		return id;
@@ -108,4 +115,14 @@ public class User implements Serializable{
 	public String getPassword() {
 		return password;
 	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
+	
 }
