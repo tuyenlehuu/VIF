f4b0509aaf1fe4d122807f6c5b65bb7fec404cb3
Tuyenlh <tuyen.le.huu@bluebelt.asia>
2019-03-26 17:39:24 +0700

    implement invest history function

--- VIF-Frontend/src/app/views/customer-invest-history/cus-invest.history.component.ts
+++ VIF-Frontend/src/app/views/customer-invest-history/cus-invest.history.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { first, catchError } from 'rxjs/operators';
+import { first } from 'rxjs/operators';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
 import { config } from '../../config/application.config';
@@ -8,6 +8,8 @@ import { CustomerService } from '../../services/customer.service';
 import { Customer } from '../../models/Customer.model';
 import { InvestorTransService } from '../../services/investor.transaction.service';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { InvestorHistory } from '../../models/InvestorHistory.model';
+import { Pager } from '../../models/Pager';
 
 @Component({
     templateUrl: 'cus-invest-his.component.html',
@@ -19,6 +21,14 @@ export class CusInvestHistoryComponent implements OnInit {
     date: any;
     bsConfig: Partial<BsDatepickerConfig>;
     colorTheme = "theme-blue";
+    investorHisLst: InvestorHistory[] = [];
+    customerSelectedId: number;
+    fromDate: string;
+    toDate: string;
+    p: number = 1;
+    total: number;
+    pageSize: number = 5;
+
 
     constructor(private toastrService: ToastrService, private customerService: CustomerService, private investorTransService: InvestorTransService) {      
         this.bsConfig = Object.assign({}, { containerClass: this.colorTheme });
@@ -29,6 +39,7 @@ export class CusInvestHistoryComponent implements OnInit {
             // console.log("data: ", respons);
             this.customers = respons;
         });
+        this.search();
     }
 
     showSuccess(mes: string) {
@@ -40,6 +51,22 @@ export class CusInvestHistoryComponent implements OnInit {
     showError(mes: string) {
         this.toastrService.error('', mes, {
             timeOut: config.timeoutToast
+        });
+    }
+
+    search(){
+        this.getPage(1);
+    }
+
+    getPage(page: number) {
+        var pager: Pager = new Pager();
+        pager.page = page;
+        pager.pageSize = this.pageSize;
+        this.investorTransService.searchInvestorHistoryByCondition(this.customerSelectedId, this.fromDate, this.toDate, pager).pipe(first()).subscribe((respons: any) => {
+            this.investorHisLst = respons.data;
+            this.total = respons.totalRow;
+            this.p = page;
+            // console.log("data: ", respons);
         });
     }
 
