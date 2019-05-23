28a621fa0c810fad1a9f9257bab17b9389f327e8
Tran Quang Duc <duc01629347115@gmail.com>
2019-05-23 23:14:45 +0700

    update config done

--- VIF-Frontend/src/app/views/appParam/appParam.component.ts
+++ VIF-Frontend/src/app/views/appParam/appParam.component.ts
@@ -20,6 +20,7 @@ export class AppParamComponent implements OnInit {
     propKey : string;
     propValue : string;
     description: string;
+    propType: string;
 
     status = [
         {
@@ -33,21 +34,6 @@ export class AppParamComponent implements OnInit {
         {
             name: 'Ngừng hoạt động',
             value: 0 && null
-        }
-    ];
-
-    propType = [
-        {
-            name: 'Chọn loại',
-            value: '0'
-        },
-        {
-            name: 'Phí giao dịch CCQ',
-            value: '1'
-        },
-        {
-            name: 'Phí giao dịch chứng khoán',
-            value: '2'
         }
     ];
 
@@ -98,6 +84,9 @@ export class AppParamComponent implements OnInit {
     }
 
     enterOnSubmitPropKey(){
+        this.getPage(1);
+    }
+    enterOnSubmitPropType(){
         this.getPage(1);
     }
     enterOnSubmitPropValue(){
