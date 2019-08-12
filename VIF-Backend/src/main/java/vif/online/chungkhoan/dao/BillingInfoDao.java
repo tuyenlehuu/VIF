7971e5c9b0b9ba404512cbf06649cf4410cc22c1
ThanLibra <41472635+ThanLibra@users.noreply.github.com>
2019-08-12 15:17:49 +0700

    set serverTomezone=utc in properties

--- VIF-Backend/src/main/java/vif/online/chungkhoan/dao/BillingInfoDao.java
+++ VIF-Backend/src/main/java/vif/online/chungkhoan/dao/BillingInfoDao.java
@@ -1,22 +1,22 @@
-//package vif.online.chungkhoan.dao;
-//
-//import java.util.List;
-//
-//import vif.online.chungkhoan.entities.BillingInfo;
-//
-//public interface BillingInfoDao {
-//	List<BillingInfo> getAlls();
-//
-//	BillingInfo getById(int id);
-//
-//	void deleteById(Integer id);
-//
-//	void updateBillingInfo(BillingInfo bInfo);
-//
-//	boolean addBillingInfo(BillingInfo bInfo);
-//
-//	List<BillingInfo> searchByCondition(int page, int pageSize, Boolean asc, String accountName, String bankName,
-//			String bankBranch, String bankAccount, Integer activeFlg);
-//
-//	int getRowCount(String accountName, String bankName, String bankBranch, String bankAccount, Integer activeFlg);
-//}
+package vif.online.chungkhoan.dao;
+
+import java.util.List;
+
+import vif.online.chungkhoan.entities.BillingInfo;
+
+public interface BillingInfoDao {
+	List<BillingInfo> getAlls();
+
+	BillingInfo getById(int id);
+
+	void deleteById(Integer id);
+
+	void updateBillingInfo(BillingInfo bInfo);
+
+	boolean addBillingInfo(BillingInfo bInfo);
+
+	List<BillingInfo> searchByCondition(int page, int pageSize, Boolean asc, String accountName, String bankName,
+			String bankBranch, String bankAccount, Integer activeFlg);
+
+	int getRowCount(String accountName, String bankName, String bankBranch, String bankAccount, Integer activeFlg);
+}
--- VIF-Backend/src/main/java/vif/online/chungkhoan/entities/BillingInfo.java
+++ VIF-Backend/src/main/java/vif/online/chungkhoan/entities/BillingInfo.java
@@ -1,122 +1,122 @@
-//package vif.online.chungkhoan.entities;
-//
-//import java.io.Serializable;
-//import java.util.Date;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.FetchType;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.ManyToOne;
-//import javax.persistence.Table;
-//import javax.persistence.Temporal;
-//import javax.persistence.TemporalType;
-//@Entity
-//@Table(name = "billing_info")
-//public class BillingInfo implements Serializable{
-//
-//	/**
-//	 * 
-//	 */
-//	private static final long serialVersionUID = 1L;
-//
-//	@Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "ID")
-//    private Integer id;
-//	
-//	@Column(name="ACCOUNT_NAME")
-//	private String accountName;
-//	
-//	@Column(name="BANK_NAME")
-//	private String bankName;
-//	
-//	@Column(name="BANK_BRANCH")
-//	private String bankBranch;
-//	
-//	@Column(name="BANK_ACCOUNT")
-//	private String bankAccount;
-//	
-//	@Column(name="ACTIVE_FLG")
-//	private int activeFlg=1;
-//	
-//	@Column(name="UPDATE_DATE", columnDefinition = "DATETIME")
-//	@Temporal(TemporalType.TIMESTAMP)
-//	private Date updateDate;
-//	
-//	@ManyToOne(fetch = FetchType.EAGER)
-//    @JoinColumn(name = "customer_id")
-//    private Customer customer;
-//
-//	public Integer getId() {
-//		return id;
-//	}
-//
-//	public void setId(Integer id) {
-//		this.id = id;
-//	}
-//
-//	public String getAccountName() {
-//		return accountName;
-//	}
-//
-//	public void setAccountName(String accountName) {
-//		this.accountName = accountName;
-//	}
-//
-//	public String getBankName() {
-//		return bankName;
-//	}
-//
-//	public void setBankName(String bankName) {
-//		this.bankName = bankName;
-//	}
-//
-//	public String getBankBranch() {
-//		return bankBranch;
-//	}
-//
-//	public void setBankBranch(String bankBranch) {
-//		this.bankBranch = bankBranch;
-//	}
-//
-//	public String getBankAccount() {
-//		return bankAccount;
-//	}
-//
-//	public void setBankAccount(String bankAccount) {
-//		this.bankAccount = bankAccount;
-//	}
-//
-//	public Date getUpdateDate() {
-//		return updateDate;
-//	}
-//
-//	public void setUpdateDate(Date updateDate) {
-//		this.updateDate = updateDate;
-//	}
-//
-//	public Customer getCustomer() {
-//		return customer;
-//	}
-//
-//	public void setCustomer(Customer customer) {
-//		this.customer = customer;
-//	}
-//
-//	public int getActiveFlg() {
-//		return activeFlg;
-//	}
-//
-//	public void setActiveFlg(int activeFlg) {
-//		this.activeFlg = activeFlg;
-//	}
-//	
-//	
-//	
-//	
-//}
-//
+package vif.online.chungkhoan.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+@Entity
+@Table(name = "billing_info")
+public class BillingInfo implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+	
+	@Column(name="ACCOUNT_NAME")
+	private String accountName;
+	
+	@Column(name="BANK_NAME")
+	private String bankName;
+	
+	@Column(name="BANK_BRANCH")
+	private String bankBranch;
+	
+	@Column(name="BANK_ACCOUNT")
+	private String bankAccount;
+	
+	@Column(name="ACTIVE_FLG")
+	private int activeFlg=1;
+	
+	@Column(name="UPDATE_DATE", columnDefinition = "DATETIME")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date updateDate;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+	public String getBankBranch() {
+		return bankBranch;
+	}
+
+	public void setBankBranch(String bankBranch) {
+		this.bankBranch = bankBranch;
+	}
+
+	public String getBankAccount() {
+		return bankAccount;
+	}
+
+	public void setBankAccount(String bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public int getActiveFlg() {
+		return activeFlg;
+	}
+
+	public void setActiveFlg(int activeFlg) {
+		this.activeFlg = activeFlg;
+	}
+	
+	
+	
+	
+}
+
